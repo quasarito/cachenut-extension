add86bd3dd9bed1474f28252ed51a1aa1b3d903f
Huy Duong <26372511+quasarito@users.noreply.github.com>
2022-04-19 01:26:12 -0400

    Use different manifest versions by browser
    - firefox uses v2
    - chrome uses v3

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
   mode: nodeEnv,
 
   entry: {
-    manifest: path.join(sourcePath, 'manifest.json'),
+    manifest: path.join(sourcePath, 'manifest', targetBrowser === 'firefox' ? 'v2' : 'v3', 'manifest.json'),
     background: path.join(sourcePath, 'Background', 'index.ts'),
     contentScript: path.join(sourcePath, 'ContentScript', 'index.ts'),
     popup: path.join(sourcePath, 'Popup', 'index.tsx'),
